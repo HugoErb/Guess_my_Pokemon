64881dd3f0527b23dd0f712970a91a3dbe3f1469
Developer <dev@guess-my-pokemon.local>
2026-04-13 16:30:26 +0200

    simplification du redeploiement

--- redeploiement.sh
+++ redeploiement.sh
@@ -1,48 +1,21 @@
 #!/bin/bash
 
-# Charger NVM et rendre npm/pm2 disponibles dans un contexte systemd
 export NVM_DIR="/home/ubuntu/.nvm"
 export PATH="$NVM_DIR/versions/node/v22.22.2/bin:$PATH"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Sortie console + fichier log
 exec > >(tee -a /home/ubuntu/Guess_my_Pokemon/redeploy.log) 2>&1
-echo "======== $(date) | Déploiement de Guess my Pokemon démarré ========"
+echo "======== $(date) | Déploiement Guess my Pokemon ========"
 
-# Aller dans le bon dossier
-cd /home/ubuntu/Guess-my-Pokemon || exit 1
+cd /home/ubuntu/Guess_my_Pokemon || exit 1
 
-# Variables
-nomApplication="Guess-my-Pokemon"
-NPM_CMD=$(which npm)
-PM2_CMD=$(which pm2)
-
-if [ -z "$NPM_CMD" ] || [ -z "$PM2_CMD" ]; then
-    echo "Erreur : npm ou pm2 est introuvable dans l'environnement NVM"
-    exit 1
-fi
-
-# Mise à jour du dépôt
 echo "Mise à jour du dépôt Git..."
 git pull origin
 
-# Installation des dépendances
 echo "Installation des dépendances..."
-$NPM_CMD install
+npm install
 
-# Build
 echo "Lancement du build..."
-$NPM_CMD run build
+npm run build
 
-# Lancement/redémarrage avec PM2
-echo "(Re)démarrage de l'application '$nomApplication' via PM2..."
-$PM2_CMD startOrRestart ecosystem_production.config.js --only "$nomApplication"
-
-if $PM2_CMD describe "$nomApplication" > /dev/null; then
-    echo "Application '$nomApplication' active après startOrRestart."
-else
-    echo "Erreur : L'application '$nomApplication' n'a pas pu être lancée."
-    exit 1
-fi
-
-echo "======== Déploiement de Guess my Pokemon terminé avec succès ========"
+echo "======== Déploiement terminé avec succès ========"
