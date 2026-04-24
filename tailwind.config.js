45e51c336b5c427f465508334c54bb274b5cfee4
HugoErb <erb.hugo@yahoo.fr>
2026-04-24 23:02:12 +0200

    Fix phone layout on tablets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    screens: {
+      sm:  '640px',
+      md:  '1024px',
+      lg:  '1280px',
+      xl:  '1536px',
+      '2xl': '1792px',
+    },
     extend: {},
   },
   plugins: [],
