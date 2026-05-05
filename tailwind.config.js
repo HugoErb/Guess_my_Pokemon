ac58874f959a68bc899f6df43354136c0437d74f
Developer <dev@guess-my-pokemon.local>
2026-05-05 17:41:15 +0200

    modif effet hover en mode tel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     screens: {
       sm:  '640px',
