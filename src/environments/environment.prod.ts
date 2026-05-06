3fbb362bf607e4583446679538deb6c615c95c59
Developer <dev@guess-my-pokemon.local>
2026-05-06 19:30:37 +0200

    maj gitinore + maj ddb schema

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+	production: true,
+	devMode: false,
+	supabaseUrl: 'https://dilpsgggbycjdfxvygsl.supabase.co',
+	supabaseAnonKey:
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpbHBzZ2dnYnljamRmeHZ5Z3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MjQ2NjQsImV4cCI6MjA5MTUwMDY2NH0.U5ooquSznHbROAsfqzi-yxTlHbkZiQVrB2q51AmZptM',
+};
